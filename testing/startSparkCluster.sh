c6920b0e546784a30ead1371346e49fe98556618
epahomov <pahomov.egor@gmail.com>
2014-12-12 02:57:45 +0200

    Just rerun CI

--- testing/startSparkCluster.sh
+++ testing/startSparkCluster.sh
@@ -4,6 +4,7 @@ tar zxvf spark-1.1.1-bin-hadoop2.3.tgz
 cd spark-1.1.1-bin-hadoop2.3
 export SPARK_MASTER_PORT=7071
 export SPARK_MASTER_WEBUI_PORT=7072
+
 ./sbin/start-master.sh
 ./bin/spark-class org.apache.spark.deploy.worker.Worker spark://epahomov-osx.local:7071 &> worker.log &
 ./bin/spark-class org.apache.spark.deploy.worker.Worker spark://epahomov-osx.local:7071 &> worker2.log &
