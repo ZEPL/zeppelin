4fbf6fa7e08acfc8705d8c73c9dfa3e172d41cd4
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2015-04-26 11:11:59 +0200

    The connected or anonymous user should get a ticket first from this API

--- zeppelin-server/src/main/java/com/nflabs/zeppelin/rest/SecurityRestApi.java
+++ zeppelin-server/src/main/java/com/nflabs/zeppelin/rest/SecurityRestApi.java
@@ -1,19 +1,16 @@
 package com.nflabs.zeppelin.rest;
 
-import com.nflabs.zeppelin.interpreter.InterpreterSetting;
-import com.nflabs.zeppelin.rest.message.InterpreterSettingListForNoteBind;
 import com.nflabs.zeppelin.server.JsonResponse;
 import com.nflabs.zeppelin.session.TicketContainer;
 import org.apache.shiro.SecurityUtils;
-import scala.Tuple2;
+import org.apache.shiro.subject.Subject;
 
-import javax.security.auth.Subject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Zeppelin root rest api endpoint.
@@ -33,13 +30,20 @@ public class SecurityRestApi {
 
   /**
    * Get ticket
-   *
+   * Returns username & ticket
+   * for anonymous access, username is always anonymous.
+   * After getting this ticket, access through websockets become safe
    * @return 200 response
    */
   @GET
   @Path("ticket")
   public Response ticket() {
-    String principal = SecurityUtils.getSubject().getPrincipal().toString();
+    Object oprincipal = SecurityUtils.getSubject().getPrincipal();
+    String principal;
+    if (oprincipal == null)
+      principal = "anonymous";
+    else
+      principal = oprincipal.toString();
     String ticket = TicketContainer.instance.getTicket(principal);
     Map<String, String> data = new HashMap<>();
     data.put("principal", principal);
